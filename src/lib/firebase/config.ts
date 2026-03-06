7911b20faab22c28af24d62dbde23080e4144771
Technology iGen <igentech1@gmail.com>
2026-03-06 10:46:22 +0000

    tại sao khi đăng nhập bằng tài khoản admin có quyền cao nhất là igen-per

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -6,19 +6,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBhEmODPzTASWIw5nP68AWaKAueiP8g-9c",
+  authDomain: "studio-5835932949-38ba9.firebaseapp.com",
+  projectId: "studio-5835932949-38ba9",
+  storageBucket: "studio-5835932949-38ba9.appspot.com",
+  messagingSenderId: "732004220860",
+  appId: "1:732004220860:web:5dee1a1aeed6714184f5b9",
 };
-
-if (!firebaseConfig.apiKey || !firebaseConfig.projectId) {
-  throw new Error(
-    'Firebase configuration is missing or incomplete. Please check your environment variables (e.g., .env or .env.local) and ensure that NEXT_PUBLIC_FIREBASE_API_KEY and NEXT_PUBLIC_FIREBASE_PROJECT_ID are set correctly.'
-  );
-}
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
